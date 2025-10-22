c0b5093514c24b93d7b8785968b0be8d9ae72806
RAJIV KUMAR <rajivji13042004@gmail.com>
2025-10-22 23:44:55 +0530

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+    
+    std::srand(std::time(0));
+
+    
+    int randomNumber = std::rand() % 100 + 1;
+    int guess = 0;
+
+    std::cout << "Guess the number between 1 and 100.";
+
+    while (guess != randomNumber) {
+        std::cout << "Enter your guess: ";
+        std::cin >> guess;
+
+        if (guess > randomNumber) {
+            std::cout << "Too high! Try again.";
+        } else if (guess < randomNumber) {
+            std::cout << "Too low! Try again.";
+        } else {
+       std::cout << "Congratulations! You guessed the correct number.";
+        }
+    }
+
+    return 0;
+}
